3e52784dfeac59fd91d5e8acedc5d74b0ecf4654
nhan-nguyen <tuong-nhan.nguyen-dong@insa-lyon.fr>
2012-03-28 08:52:43 +0200

    result

---
+++ result/calcul_couleur_total.bat
@@ -0,0 +1,10 @@
+calcul_couleur_90-100.bat
+calcul_couleur_10-19.bat
+calcul_couleur_20-29.bat
+calcul_couleur_30-39.bat
+calcul_couleur_40-49.bat
+calcul_couleur_50-59.bat
+calcul_couleur_60-69.bat
+calcul_couleur_70-79.bat
+calcul_couleur_80-89.bat
+
